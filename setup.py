bc5d7c617ced1213e4ebecbde64381e2e3f23ddc
a146874 <christopher_defreitas@progressive.com>
2020-12-23 15:07:33 -0500

    created setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'example-pkg=A146874',
+    version = '0.0.1',
+    author = "Chris Defreitas",
+    author_email = "christopher_defreitas@progressive.com",
+    description = "A sample package",
+    long_description = long_description,
+    url="https://github.com/A146874/example-pkg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
